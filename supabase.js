f34be4e718ce48b87a674740c066bb0a0fb1902c
igameventures-dot <igameventures@gmail.com>
2026-09-16 13:00:47 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+// supabase.js
+
+const SUPABASE_URL = "https://mllgwshzpvjqexorhoir.supabase.co";
+const SUPABASE_KEY = "sb_publishable_BaZT-LtVLabRUDfX0tRtvg_cVQR20wW";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
